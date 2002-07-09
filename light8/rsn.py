a7f357f1fc931e610bfd227120a1f1f0ebaa3343
drewp <none>
2002-07-08 23:34:29 -0700

    rm'd unneeded imports
    rm'd unneeded imports
    profiled
    
    darcs-hash:20020709063429-ecde5-bf370399c3dd84a60db80c19930835160cacdb74.gz

--- light8/rsn.py
+++ light8/rsn.py
@@ -2,22 +2,11 @@
 from __future__ import nested_scopes
 
 from Tix import *
-from time import sleep
 from signal import signal, SIGINT
-import sys, cPickle
-
 import io
 from uihelpers import *
-from panels import *
-from Xfader import *
-from subediting import Subediting
 from Fader import Fader
-import stage
 from Lightboard import Lightboard
-
-import Subs, Patch
-
-
 
 if len(sys.argv) >= 2:
     DUMMY = 0
@@ -33,7 +22,6 @@ root.wm_title(window_title)
 root.wm_geometry('+462+470')
 root.tk_focusFollowsMouse()
 
-
 parportdmx = io.ParportDMX(DUMMY)
 
 mr_lightboard = Lightboard(root,parportdmx,DUMMY)
@@ -41,4 +29,8 @@ mr_lightboard = Lightboard(root,parportdmx,DUMMY)
 signal(SIGINT, mr_lightboard.quit)
 bindkeys(root,'<Escape>', mr_lightboard.quit)
 
+
 root.mainloop() # Receiver switches main
+
+#import profile
+#profile.run("root.mainloop()","profile/idlemanysubs")
