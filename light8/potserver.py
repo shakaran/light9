bc61ce18da452b803312365731dd83e9029e0143
drewp <none>
2002-07-12 19:57:55 -0700

    messed with the server a lot. it works, though doesnt do persistent conenctions
    darcs-hash:20020713025755-ecde5-f7fe784fafc264bd0978d3f9a4f45fa92bf7bbd3.gz

--- light8/potserver.py
+++ light8/potserver.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python
 
-import socket
+import socket,time
 
 from io import *
 
 pots = SerialPots()
 pots.golive()
 
+laste=""
 while 1:
+    
     l=pots.getlevels()
+        
     try:
         s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(("dash", socket.getservbyname('serpots','tcp')))
-
+        s.setblocking(1)
+        ret=s.connect_ex(("dash", socket.getservbyname('rlslider','tcp')))
+#        print ret        
         s.send("%d %d %d %d\n" % l)
         s.close()
     except Exception,e:
-        print "Exception: %s" % e
+        print str(e)
+        s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if ret==111:
+            print time.ctime(),"waiting for server"
+            time.sleep(3)
+        else:
+            print time.ctime(),"connected"
+
 
 
