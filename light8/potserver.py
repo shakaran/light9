bac6ab869895ed31650ed78cbd7e4b42b8db8958
dmcc <none>
2002-07-20 21:38:19 -0700

    results of 7.20 show, potserver and tracks finally in CVS
    darcs-hash:20020721043819-13ce2-976518823d5407e00880cf9b7dbf90cacf37848f.gz

--- light8/potserver.py
+++ light8/potserver.py
@@ -8,14 +8,45 @@ pots = SerialPots()
 pots.golive()
 
 laste=""
+lastlevs=(0,0,0,0)
+dirs=[0,0,0,0]
+samples=nsends=noises=0
+watch=0
 while 1:
-    
+    if samples > 30:
+        fps=1.0*samples/(time.time()-watch)
+        watch=time.time()
+        print "S"*nsends+"n"*noises+" "*(samples-nsends-noises),"%.1f Hz"%fps
+        samples=nsends=noises=0
+    samples+=1
     l=pots.getlevels()
-        
+    
+    # no change at all?
+    if l==lastlevs:
+        time.sleep(.01)    
+        continue
+
+    report=0 # we only will report if a dimmer moves twice in the same direction
+    for i in range(0,4):
+        change = l[i]-lastlevs[i]
+        if change!=0:
+            thisdir = (change>0)-(change<0)
+            if thisdir==dirs[i]:
+                # a dimmer is moving in a constant direction
+                report=1
+            dirs[i]=thisdir
+
+    if report==0:
+        noises+=1
+        continue
+    
+    lastlevs = l
+    nsends+=1
+
     try:
         s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.setblocking(1)
-        ret=s.connect_ex(("dash", socket.getservbyname('rlslider','tcp')))
+        #s.setblocking(1)
+        ret=s.connect_ex(("10.1.0.32", socket.getservbyname('rlslider','tcp')))
 #        print ret        
         s.send("%d %d %d %d\n" % l)
         s.close()
@@ -26,7 +57,7 @@ while 1:
             print time.ctime(),"waiting for server"
             time.sleep(3)
         else:
-            print time.ctime(),"connected"
+            print time.ctime(),e
 
 
 
