10964b71efff524b334576831a3b31a6d62873a8
dmcc <none>
2003-06-14 07:54:36 -0700

    tiny cleanups and reload on import
    darcs-hash:20030614145436-13ce2-e4f0da3dc0a9bc12651f94b2c0b46ea7c105f32f.gz

--- light8/Patch.py
+++ light8/Patch.py
@@ -1,17 +1,12 @@
-from types import TupleType
-
-
 def resolve_name(channelname):
     "Ensure that we're talking about the primary name of the light."
     return get_channel_name(get_dmx_channel(channelname))
-
 
 def get_all_channels():
     """returns primary names for all channels (sorted)"""
     prinames = reverse_patch.values()[:]
     prinames.sort()
     return prinames
-        
 
 def get_dmx_channel(name):
     if name in patch:
@@ -29,22 +24,23 @@ def get_channel_name(dmxnum):
     except KeyError:
         return str(dmxnum)
 
-def reload_data(dummy):
+def reload_data():
     global patch, reverse_patch
-    if dummy:
-        import ConfigDummy as Config
-    else:
-        import Config
+    import patchdata
 
-    reload(Config)
-    loadedpatch = Config.patch
+    reload(patchdata)
+    loadedpatch = patchdata.patch
     patch = {}
     reverse_patch = {}
     for k, v in loadedpatch.items():
-        if type(k) == TupleType:
+        if type(k) is tuple:
             for name in k:
                 patch[name] = v
             reverse_patch[v] = k[0]
         else:
             patch[k] = v
             reverse_patch[v] = k
+
+# importing patch will load initial data
+reload_data()
+
