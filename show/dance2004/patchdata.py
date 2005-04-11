6876e836b2349e5dd1e0f5860dcb5c69ad0766ae
drewp <drewp@bigasterisk.com>
2005-04-10 18:55:37 -0700

    put correct patchdata in 2004 show
    darcs-hash:20050411015537-312f9-efd72c9e37ad881fbd714fa215c56ac1929b496d.gz

--- show/dance2004/patchdata.py
+++ show/dance2004/patchdata.py
@@ -1,56 +1,56 @@
 patch = {
     
-    ('side l','sidepost1') : 45, # posts
-    ('side r','sidepost2') : 46,
-    
-    ('god',) : 6,
-    
-    'house': 68,
-    ('ramp1','dolly-ramp-l','b11',): 54, # left bank over house
-    ('main-l-blue', 'b12',): 53,
-    ('ramp2', 'b13',): 52,
-    ('main-lc-fill', 'b14',): 51,
-    ('main-c1', 'b15',): 50,
-    ('main-r-red','b16',): 49,
-    ('ramp6','b21',): 55, # mid bank
-    ('ramp5','b22',): 56,
-    ('ramp0-stairs','b23',): 57,
-    ('main-c2', 'b24',): 58,
-    ('ramp9-stairs', 'stairs-r', 'b25',): 59,
-    ('ramp3', 'b26',): 60,
-    ('ramp4', 'b31',): 61, # right bank
-    ('main-l-red', 'b32',): 62,
-    ('main-c3', 'b33',): 63,
-    ('main-r-blue', 'b34',): 64,
-    ('ramp8', 'dolly-ramp-r', 'b35',) : 65,
-    ('ramp7', 'b36',): 66,
+#    ('side l','sidepost1') : 45, # posts
 
     'oran1':21,    'oran2':25,    'oran3':29,    'oran4':33,
     'gree1':22,    'gree2':26,    'gree3':30,    'gree4':34,
     'blue1':23,    'blue2':27,    'blue3':31,    'blue4':35,
     'red1' :24,    'red2' :28,    'red3' :32,    'red4' :36,
 
-    # hello dolly
-    'f1' : 1,
-    'f1.5' : 3,
-    'f2' : 4,
-    'f3' : 5,
-    'f4' : 7,
-    'f5' : 8,
-    'diag' : 9,
-    'f6' : 10,
-    'diag-back' : 11,
-    'f7' : 12,
+    ('edge-l',) : 52,
+    ('edge-r',) : 64,
 
-    'downfill-left' : 13,
-    'downfill-right' : 14,
-    'l-scp' : 40,
-    'r-scp' : 37,
-    'upstairs above' : 39,
-    'upstairs front' : 15,
-    'storefill' : 17,
-    'dayback' : 41,
-    'judge' : 19,
-    ('c-scp', 'c-scoop') : 38,
-    'candles' : 42,
+    ('red-c',) : 53,
+
+    ('stairs-c',) : 55,
+    ('sharlyn-c',) : 60,
+
+    ('floor1',) : 59,
+    ('floor2',) : 57,
+    ('floor3',) : 56,
+    ('floor4',) : 62,
+
+    ('deep1',) : 51,
+    ('deep2',) : 54,
+    ('deep3-red',) : 65,
+    ('deep4',) : 63,
+    ('deep5',) : 50,
+    ('deep6',) : 49,
+    ('deep7-warm',) : 66,
+
+    'post-l':45,
+    'post-r':46,
+
+    'down-front-sides':16,
+    'down-front-c':18,
+    'down-l':13,
+    'down-r':14,
+    'down-back1':39,
+    'down-back2':40,
+
+    'front1':2,
+    'front2':3,
+    'front3':4,
+    'front4':5,
+    'front5':9,
+    'front6':11,
+    'front7':8,
+    'front8':12,
+
+
+    'cyc' : 44,
+
+    'blacklight':61,
+    'house':68,
+
 }
