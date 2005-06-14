e7975e6dd64851fd91e6321e5467eff0e250d819
drewp <drewp@bigasterisk.com>
2005-06-13 19:52:37 -0700

    finished incomplete patch- asco now measures the length of songs (only for .wav)
    darcs-hash:20050614025237-312f9-2bc9e2b16201f734cef285fcdfaea2da3db8549d.gz

---
+++ light9/wavelength.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+from __future__ import division, nested_scopes
+import sys, wave
+
+def wavelength(filename):
+    wavefile = wave.open(filename, 'rb')
+
+    framerate = wavefile.getframerate() # frames / second
+    nframes = wavefile.getnframes() # number of frames
+    song_length = nframes / framerate
+
+    return song_length
+
+if __name__ == "__main__":
+    for songfile in sys.argv[1:]:
+        print songfile, wavelength(songfile)
