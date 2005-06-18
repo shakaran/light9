9a200abdb5c7b17cea1e07f88785abc5464e0dab
David McClosky <dmcc@bigasterisk.com>
2005-06-18 09:59:04 -0700

    chase logic is (mostly) fixed, integrate with curvecalc
    Also add some quick names for chases
    
    darcs-hash:20050618165904-78b18-ed33426b530ca21c2032a014114e4a3cccecb9b3.gz

--- light9/chase.py
+++ light9/chase.py
@@ -1,13 +1,15 @@
 from __future__ import division
 
-def chase(t, ontime=0.5, offtime=0.5, offset=0.2, onval=1.0, 
+def chase(t, ontime=0.5, offset=0.2, onval=1.0, 
           offval=0.0, names=None, combiner=max):
     names = names or []
-    period = ontime + offtime
+    # maybe this is better:
+    # period = ontime + ((offset + ontime) * (len(names) - 1))
+    period = (offset + ontime) * len(names)
     outputs = {}
     for index, name in enumerate(names):
         # normalize our time
-        local_offset = offset * index
+        local_offset = (offset + ontime) * index
         local_t = t - local_offset
         local_t %= period
 
@@ -19,7 +21,7 @@ def chase(t, ontime=0.5, offtime=0.5, offset=0.2, onval=1.0,
 
         # it could be in there twice (in a bounce like (1, 2, 3, 2)
         if name in outputs:
-            outputs[name] = max(value, outputs[name])
+            outputs[name] = combiner(value, outputs[name])
         else:
             outputs[name] = value
     return outputs
@@ -28,7 +30,7 @@ if __name__ == "__main__":
     # a little testing
     for x in range(80):
         x /= 20.0
-        output = chase(x, onval='x', offval=' ', ontime=0.3, offtime=0.6,
+        output = chase(x, onval='x', offval=' ', ontime=0.1, offset=0.2,
                        names=('a', 'b', 'c', 'd'))
         output = output.items()
         output.sort()
