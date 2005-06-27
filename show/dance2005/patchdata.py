ec0ad158c3ea68151e36a37130b082ce19622649
Drew Perttula <drewp@bigasterisk.com>
2005-06-26 18:46:39 -0700

    add final show data for dance2005
    darcs-hash:20050627014639-836b1-bb8fcedc28a88eafeb1e878e95fb9bc7f5668922.gz

---
+++ show/dance2005/patchdata.py
@@ -0,0 +1,60 @@
+patch = {
+    
+#    ('side l','sidepost1') : 45, # posts
+
+    'oran1':21,    'oran2':25,    'oran3':29,    'oran4':33,
+    'gree1':22,    'gree2':26,    'gree3':30,    'gree4':34,
+    'blue1':23,    'blue2':27,    'blue3':31,    'blue4':35,
+    'red1' :24,    'red2' :28,    'red3' :32,    'red4' :36,
+
+
+    ('back9','b11',) : 54,
+    ('back6','b12',) : 53,
+    ('back4','b13',) : 52,
+    ('back1','b14',) : 51,
+    ('back3','b15',) : 50,
+    ('back7','b16',) : 49,
+    
+    ('b21broke',) : 55,
+    ('lowerstage-l','b22',) : 56,
+    ('patio-l','b23',) : 57,
+    ('patio-r','b24',) : 58,
+    ('lowerstage-r','b25',) : 59,
+    ('shar','b26',) : 60,
+    
+    ('back2','b31',) : 61,
+    ('back8','b32',) : 62,
+    ('b33broke',) : 63,
+    ('back10','b34',) : 64,
+    ('b35broke',) : 65,
+    ('back5','b36',) : 66,
+
+    'post-l':45,
+    'post-r':46,
+
+    'down-sides':16,
+    'down-c':18,
+    'down-l':13,
+    'down-r':14,
+#    'down-back1':39,
+    'up-l':40,
+
+    'front1-2':2,
+    'front2':3,
+    'front3':4,
+    'front4':5,
+    'god' : 6,
+    'front5' : 7,    
+    'front6':9,
+    'front7':10,
+    'front8':12,
+
+    'blacklight': 15,
+    'stroberate':19,
+    'strobedim':20,
+
+    'cyc' : 44,
+
+    'house':68,
+
+}
