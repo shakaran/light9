e4a9ade41be631cf4de31f1f337391f4779d3f57
Drew Perttula <drewp@bigasterisk.com>
2006-06-16 09:59:30 -0700

    music path changes
    darcs-hash:20060616165930-836b1-3b88d6a5bc52c796aadaedc2d8d5103cca13487c.gz

--- light9/showconfig.py
+++ light9/showconfig.py
@@ -16,38 +16,21 @@ def root():
     return r
 
 def musicDir():
-    return path.join(root(),"music")
+    return path.join(root(),"music_local")
 
 def songInMpd(song):
 
     """mpd only works off its own musicroot, which for me is
     /my/music. song is a file in musicDir; this function returns a
     version starting with the mpd path, but minus the mpd root itself.
-    the mpc ~/.mpdconf """
+    the mpc ~/.mpdconf
 
-    assert isinstance(song, URIRef), "songInMpd now takes URIRefs"
-
-    mpdHome = None
-    for line in open(path.expanduser("~/.mpdconf")):
-        if line.startswith("music_directory"):
-            mpdHome = line.split()[1].strip('"')
-    if mpdHome is None:
-        raise ValueError("can't find music_directory in your ~/.mpdconf")
-    mpdHome = mpdHome.rstrip(path.sep) + path.sep
-
-    songFullPath = songOnDisk(song)
-    if not songFullPath.startswith(mpdHome):
-        raise ValueError("the song path %r is not under your MPD music_directory (%r)" % (songFullPath, mpdHome))
-        
-    mpdRelativePath = songFullPath[len(mpdHome):]
-    if path.join(mpdHome, mpdRelativePath) != songFullPath:
-        raise ValueError("%r + %r doesn't make the songpath %r" % (mpdHome, mpdRelativePath, songFullPath))
-    return mpdRelativePath.encode('ascii')
-
-def songOnDisk(song):
-    graph = getGraph()
-    songFullPath = path.join(root(), graph.value(song, L9['showPath']))
-    return songFullPath
+    changed root to /home/drewp/projects/light9/show/dance2005 for now
+    """
+    
+    if 'dance2005' in root():
+        return "projects/dance2005/%s" % song
+    raise NotImplementedError
 
 def curvesDir():
     return path.join(root(),"curves")
