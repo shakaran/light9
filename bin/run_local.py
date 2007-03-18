079eed7e55ab75d3369add58a786db9f80bcdd02
Drew Perttula <drewp@bigasterisk.com>
2007-03-18 16:46:03 -0700

    bigger traceback printouts for all tools
    darcs-hash:20070318234603-836b1-239e1435d1c0f6c16d73118396b37a6e1fb87030.gz

--- bin/run_local.py
+++ bin/run_local.py
@@ -5,4 +5,16 @@
 import sys,os
 sys.path.insert(0,os.path.join(os.path.dirname(sys.argv[0]),".."))
 
+import cgitb
+from twisted.python.failure import Failure
 
+import Tkinter
+def rce(self, exc, val, tb):
+    sys.stderr.write("Exception in Tkinter callback\n")
+    if True:
+        sys.excepthook(exc, val, tb)
+    else:
+        Failure(val, exc, tb).printDetailedTraceback()
+Tkinter.Tk.report_callback_exception = rce
+
+cgitb.enable(format='txt')
