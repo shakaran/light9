2f060b222111303b87f89d8a4c767344257a11be
drewp <drewp@bigasterisk.com>
2007-03-19 19:34:28 -0700

    merge some rdf changes
    darcs-hash:20070320023428-312f9-c752592adf64c756d0e48ed703f4168c738228d8.gz

--- light9/showconfig.py
+++ light9/showconfig.py
@@ -24,10 +24,30 @@ def songInMpd(song):
     /my/music. song is a file in musicDir; this function returns a
     version starting with the mpd path, but minus the mpd root itself.
     the mpc ~/.mpdconf """
-    
-    if 'dance2005' in root():
-        return "projects/dance2005/%s" % song
-    raise NotImplementedError
+
+    assert isinstance(song, URIRef), "songInMpd now takes URIRefs"
+
+    mpdHome = None
+    for line in open(path.expanduser("~/.mpdconf")):
+        if line.startswith("music_directory"):
+            mpdHome = line.split()[1].strip('"')
+    if mpdHome is None:
+        raise ValueError("can't find music_directory in your ~/.mpdconf")
+    mpdHome = mpdHome.rstrip(path.sep) + path.sep
+
+    songFullPath = songOnDisk(song)
+    if not songFullPath.startswith(mpdHome):
+        raise ValueError("the song path %r is not under your MPD music_directory (%r)" % (songFullPath, mpdHome))
+        
+    mpdRelativePath = songFullPath[len(mpdHome):]
+    if path.join(mpdHome, mpdRelativePath) != songFullPath:
+        raise ValueError("%r + %r doesn't make the songpath %r" % (mpdHome, mpdRelativePath, songFullPath))
+    return mpdRelativePath.encode('ascii')
+
+def songOnDisk(song):
+    graph = getGraph()
+    songFullPath = path.join(root(), graph.value(song, L9['showPath']))
+    return songFullPath
 
 def curvesDir():
     return path.join(root(),"curves")
