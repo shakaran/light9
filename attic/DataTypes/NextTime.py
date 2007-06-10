05a00f278dd163d673eb7c68f6feb43533c9ea75
drewp <drewp@bigasterisk.com>
2007-06-09 20:39:28 -0700

    remove attic, retry patch
    darcs-hash:20070610033928-312f9-88a08c9a4c33bf75381733bd8b38a2908cfb7133.gz

--- attic/DataTypes/NextTime.py
+++
@@ -1,8 +0,0 @@
-'''Baby, there won't be no next time'''
-
-class NextTime:
-    def __init__(self, nextclock, interval=None)
-        self.nextclock = nextclock
-        self.interval = interval
-    def cylic(self):
-        return interval is not None
