1e75db0b7eaf37222d8ff57bdf5b26f5d2dd00d1
Drew Perttula <drewp@bigasterisk.com>
2012-06-07 22:11:41 -0700

    subterm display is working
    Ignore-this: 54254c7f9d6cd89bd80031ed1cf92066
    
    darcs-hash:20120608051141-836b1-e3e22e360b969fe9165e06d0dc9d23cb58b66dcd.gz

--- light9/curvecalc/subtermview.py
+++ light9/curvecalc/subtermview.py
@@ -1,40 +1,79 @@
-import Tix as tk
+import gtk
 from louie import dispatcher
 from rdflib import RDF, RDFS, Literal
 from light9 import Submaster
 from light9.namespaces import L9
 from light9.curvecalc.subterm import Subterm, Subexpr
 
+class Subexprview(object):
+    def __init__(self, se):
+        self.subexpr = se
 
-class Subexprview(tk.Frame):
-    def __init__(self,master,se,**kw):
-        self.subexpr=se
-        tk.Frame.__init__(self,master,**kw)
-        self.evar = tk.StringVar()
-        e = self.ent = tk.Entry(self,textvariable=self.evar)
-        e.pack(side='left',fill='x',exp=1)
+        self.box = gtk.HBox()
+
+        self.entryBuffer = gtk.EntryBuffer("", -1)
+        self.entry = gtk.Entry()
+        self.error = gtk.Label("")
+
+        self.box.pack_start(self.entry, expand=True)
+        self.box.pack_start(self.error, expand=False)
+
+        self.entry.set_buffer(self.entryBuffer)
         self.expr_changed()
-        self.evar.trace_variable('w',self.evar_changed)
+        self.entryBuffer.connect("deleted-text", self.evar_changed)
+        self.entryBuffer.connect("inserted-text", self.evar_changed)
         dispatcher.connect(self.expr_changed,"expr_changed",
                            sender=self.subexpr)
-        self.error = tk.Label(self)
-        self.error.pack(side='left')
-        dispatcher.connect(lambda exc: self.error.config(text=str(exc)),
-                           "expr_error",sender=self.subexpr,weak=0)
-    def expr_changed(self):
-        if self.subexpr.expr!=self.evar.get():
-            self.evar.set(self.subexpr.expr)
-    def evar_changed(self,*args):
-        self.subexpr.expr = self.evar.get()
 
-class Subtermview(tk.Frame):
-    def __init__(self, master, graph, st, **kw):
+        dispatcher.connect(lambda exc: self.error.set_text(str(exc)),
+                           "expr_error",sender=self.subexpr,weak=0)
+        
+    def expr_changed(self):
+        e = str(self.subexpr.expr)
+        if e != self.entryBuffer.get_text():
+            self.entryBuffer.set_text(e, len(e))
+            
+    def evar_changed(self,*args):
+        print "hi change"
+        self.subexpr.expr = self.entryBuffer.get_text()
+
+class Subtermview(object):
+    """
+    has .label and .exprView widgets for you to put in a table
+    """
+    def __init__(self, graph, st):
         self.subterm = st
-        tk.Frame.__init__(self,master,bd=1,relief='raised',**kw)
-        l = tk.Label(self, text="sub %s" % self.subterm.submaster.name)
-        l.pack(side='left')
-        sev=Subexprview(self,self.subterm.subexpr)
-        sev.pack(side='left',fill='both',exp=1)
+
+        self.label = gtk.Label("sub %s" % self.subterm.submaster.name)
+
+        sev = Subexprview(self.subterm.subexpr)
+        self.exprView = sev.box
+
+
+def add_one_subterm(graph, subUri, curveset, subterms, master, expr=None):
+    subname = graph.label(subUri)
+    print "%s's label is %s" % (subUri, subname)
+    if not subname: # fake sub, like for a chase
+        st = graph.subjects(L9['sub'], subUri).next()
+        subname = graph.label(st)
+        print "using parent subterm's name instead. parent %r, name %r" % (st, subname)
+    assert subname, "%s has no name" % subUri
+    if expr is None:
+        expr = '%s(t)' % subname
+
+    term = Subterm(Submaster.Submaster(graph=graph, name=subname, sub=subUri),
+                   Subexpr(curveset, expr))
+    subterms.append(term)
+
+    stv = Subtermview(graph, term)
+    y = master.get_property('n-rows')
+    master.resize(y + 1, columns=2)
+    master.attach(stv.label, 0, 1, y, y + 1, xoptions=0)
+    master.attach(stv.exprView, 1, 2, y, y + 1)
+
+    return term
+
+
 
 def makeSubtermCommandRow(master, curveset, subterms, root, ssv, graph):
     """
@@ -69,23 +108,3 @@ def makeSubtermCommandRow(master, curveset, subterms, root, ssv, graph):
     dispatcher.connect(focus_entry, "focus new subterm", weak=False)
 
     return f
-
-def add_one_subterm(graph, subUri, curveset, subterms, master, expr=None):
-    subname = graph.label(subUri)
-    print "%s's label is %s" % (subUri, subname)
-    if not subname: # fake sub, like for a chase
-        st = graph.subjects(L9['sub'], subUri).next()
-        subname = graph.label(st)
-        print "using parent subterm's name instead. parent %r, name %r" % (st, subname)
-    assert subname, "%s has no name" % subUri
-    if expr is None:
-        expr = '%s(t)' % subname
-
-    term = Subterm(Submaster.Submaster(graph=graph, name=subname, sub=subUri),
-                   Subexpr(curveset,expr))
-    subterms.append(term)
-
-    stv=Subtermview(master, graph, term)
-    stv.pack(side='top',fill='x')
-
-    return term
